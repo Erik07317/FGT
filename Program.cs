c8b2ecc13cf3cdbca201848e888535dea4be55d4
Erik Sabino <eriksabino@Eriks-MacBook-Air.local>
2025-03-31 16:49:00 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿double salario;
+Console.Write("Digite o numero do salário de funcionário: ");
+salario = double.Parse(Console.ReadLine()!);
+double FGTS;
+FGTS = 0.08*salario;
+
+Console.WriteLine($"Seu valor do sálario parcelado é {FGTS:C2}");
